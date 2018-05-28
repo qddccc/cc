109420528de1fea90694a669b803634ee73cc945
qddccc <845480506@qq.com>
2018-05-28 12:22:54 +0800

    First commit.

---
+++ app/src/main/java/com/example/providertest/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.providertest;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String newId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.example.databasetest.provider/book");
+                ContentValues values = new ContentValues();
+                values.put("name", "People");
+                values.put("author", "Davin");
+                values.put("pages", 454);
+                values.put("price",16.96);
+                Uri newUri = getContentResolver().insert(uri, values);
+                newId = newUri.getPathSegments().get(1);
+
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("content://com.example.databasetest.provider/book/"+newId);
+                ContentValues values = new ContentValues();
+                values.put("name", "Two People");
+                values.put("pages", 1216);
+                values.put("price", 24.05);
+                getContentResolver().update(uri, values, null, null);
+            }
+        });
+
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("content://com.example.databasetest.provider/book/"+newId);
+                getContentResolver().delete(uri, null, null);
+            }
+        });
+
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("content://com.example.databasetest.provider/book");
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                if(cursor != null){
+                    while(cursor.moveToNext()){
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble((cursor.getColumnIndex("price")));
+                        Log.d("MainActivity", "书名是："+name);
+                        Log.d("MainActivity", "作者是："+author);
+                        Log.d("MainActivity", "页数是："+pages);
+                        Log.d("MainActivity", "价格是："+price);
+                    }
+                    cursor.close();
+                }
+
+            }
+        });
+    }
+
+}
